37a2d00ab31a30d34bdec5295290824d34701a2d
Mipa <156837662+MipaSenpai@users.noreply.github.com>
2024-01-17 19:56:21 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,1 @@
+import mautomessage as mautomessage
---
+++ mautomessage/automessage.py
@@ -0,0 +1,15 @@
+﻿import random
+
+from . import config
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from llpy import *
+
+
+def automessage():
+    for player in mc.getOnlinePlayers():
+        player.sendText(random.choice(config.messages), config.type_message)
+
+setInterval(automessage, config.time_delay * 1000)
